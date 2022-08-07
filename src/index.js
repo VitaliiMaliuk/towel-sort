c777a140a0469e749f68aca0b514cb5731408000
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2022-08-07 12:53:48 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {
+    return [];
+  }
+  if (matrix.length === 0) {
+    return [];
+  }
+  let resultArray = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j <matrix[i].length; j++) {
+      let index = i % 2 === 0 ? j : (matrix[i].length - j - 1);
+      resultArray.push(matrix[i][index]);
+    }
+  }
+  return resultArray;
 }
